1305012b17571155c8e31c8c31d2048b6b8b66ed
Mike <mclancy96@gmail.com>
2021-05-15 16:27:59 -0400

    Create bound box css

--- smart-brain/src/components/FaceRecognition/FaceRecognition.js
+++ smart-brain/src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition'
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
     return(
         <div className="center ma">
             <div className="absolute mt2">
                 <img id="inputimage"alt='' src={imageUrl} width='500px' height='auto'></img>
+                <div className='bounding-box'></div>
             </div>
         </div>
     )
