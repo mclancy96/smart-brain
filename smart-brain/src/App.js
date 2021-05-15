678931c74836add0cbe3735e7d6b36fef20225dd
Mike <mclancy96@gmail.com>
2021-05-15 16:24:42 -0400

    Create border box calculator functions

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -30,26 +30,41 @@ class App extends Component {
     super();
     this.state = {
       input:'',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
   }
+
   onInputChange = (event) => {
     this.setState({input: event.target.value})
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - clarifaiFace.bottom_row * height
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box:box})
+  }
+
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
          this.state.input)
-      .then( 
-        (response) => {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-        },
-        (err) => {
-
-        }
-    )
+      .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
   }
   render(){
     return(
