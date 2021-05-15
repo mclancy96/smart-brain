c7447aae992555c51c4416306e4515f9c20e9973
Mike <mclancy96@gmail.com>
2021-05-15 14:16:34 -0400

    Add particles

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import './App.css';
-import Logo from './components/Logo/Logo'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Rank from './components/Rank/Rank'
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+
+const particleOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 class App extends Component {
   render(){
     return(
       <div className="App">
+        <Particles className="particles"
+          params={particleOptions}
+        />
         <Navigation/>
         <Logo/>
         <Rank/>
