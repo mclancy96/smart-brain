745dc723df1c5d0de1fb678d73f13b641b99795d
Mike <mclancy96@gmail.com>
2021-05-15 14:06:11 -0400

    Add Rank

--- smart-brain/src/components/Logo/Logo.js
+++ smart-brain/src/components/Logo/Logo.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
 import './Logo.css';
-import brain from './brain.png';
+import brain from './brain.gif';
 
 const Logo = () => {
     return(
-        <div className="ma4 mt0">
+        <div className="fl ma4 mt0 center">
             <Tilt>
-                <div className="Tilt br2 shadow-2" style={{maxWidth:"55px"}}>
-                    <img style={{paddingTop: '5px'}} alt='log' src={brain}/>
+                <div className="Tilt shadow-2 db" style={{ width:"200px"}}>
+                    <img style={{paddingTop: '3px', paddingLeft:'5px'}} alt='logo' src={brain}/>
                 </div>
             </Tilt>
+            <br></br>
         </div>
         
     )
