3f610b542ef5c1a0a063d0abd06b4c8415401ad7
Mike <mclancy96@gmail.com>
2021-05-15 17:32:42 -0400

    Setup routes complete

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
       input:'',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -71,6 +72,11 @@ class App extends Component {
   }
   
   onRouteChange = (route)  => {
+    if (route === 'signout'){
+      this.setState({isSignedIn:false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn:true})
+    }
     this.setState({route: route})
   }
 
@@ -80,7 +86,7 @@ class App extends Component {
         <Particles className="particles"
           params={particleOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         { this.state.route === 'home' ? 
           <div>
             <Logo/>
@@ -91,7 +97,7 @@ class App extends Component {
           (
             this.state.route === 'signin' ? 
             <SignIn onRouteChange={this.onRouteChange}/> :
-            <Register/> 
+            <Register onRouteChange={this.onRouteChange}/> 
           )
         } 
       </div>
--- smart-brain/src/components/Register/Register.js
+++ smart-brain/src/components/Register/Register.js
@@ -41,8 +41,8 @@ const Register = ({onRouteChange}) => {
                 onClick = {() => onRouteChange('home')}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
-                value="Sign in"
-              />
+                value="Register"
+              ></input>
             </div>
             <div className="lh-copy mt3">
             </div>
